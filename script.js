7e6fb11bab3dce27af258fc638e71e854bfb30dc
Vitor Mendes <102479412+VitorMendesGallo@users.noreply.github.com>
2022-07-09 17:44:38 -0300

    Finalizando a página Conversor

--- script.js
+++ script.js
@@ -1,14 +1,80 @@
 let opcoesMoedas = document.getElementsByName("opcoes-moedas");
-let imagemDaMoeda = document.querySelector(".imagem-moeda-selecionada");
+let botaoConverter = document.getElementById("botao-converter");
+let botaoLimpar = document.getElementById("botao-limpar");
+let valorEmReais = document.querySelector("input[type = number]");
+let resultado = document.querySelector("#resposta");
 
-function trocaImagem() {
-  if (opcoesMoedas[0].checked) {
-    imagemDaMoeda.src = "./assets/imagem-dolar.jpg";
-  } else if (opcoesMoedas[1].checked) {
-    imagemDaMoeda.src = "./assets/imagem-euro.jpg";
-  } else if (opcoesMoedas[2].checked) {
-    imagemDaMoeda.src = "./assets/imagem-libra.jpg";
-  } else {
-    imagemDaMoeda.src = "./assets/imagem-biticoin.jpg";
+(function trocaImagem() {
+  let imagemMoeda = document.querySelector(".imagem-moeda-selecionada");
+
+  for (let i = 0; i < opcoesMoedas.length; i++) {
+    opcoesMoedas[i].onclick = () => {
+      imagemMoeda.src = `./assets/imagem-${opcoesMoedas[i].value}.jpg`;
+    };
   }
+})();
+
+(function converter() {
+  botaoConverter.onclick = () => {
+    if (
+      !parseFloat(valorEmReais.value) ||
+      parseFloat(valorEmReais.value) <= 0
+    ) {
+      alert("[ATENÇÃO] DIGITE UM NÚMERO VÁLIDO !");
+    } else {
+      if (opcoesMoedas[0].checked) {
+        converteParaDolar();
+      } else if (opcoesMoedas[1].checked) {
+        converteParaEuro();
+      } else if (opcoesMoedas[2].checked) {
+        converteParaLibra();
+      } else if (opcoesMoedas[3].checked) {
+        converteParaBiticoin();
+      }
+    }
+  };
+})();
+
+function converteParaDolar() {
+  let valorReais = parseFloat(valorEmReais.value);
+  const cotacaoDolar = 5.26;
+  let valorConvertido = valorReais / cotacaoDolar;
+  resultado.innerHTML = valorConvertido.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 }
+
+function converteParaEuro() {
+  let valorReais = parseFloat(valorEmReais.value);
+  const cotacaoEuro = 5.35;
+  let valorConvertido = valorReais / cotacaoEuro;
+  resultado.innerHTML = valorConvertido.toLocaleString("de-DE", {
+    style: "currency",
+    currency: "GBP",
+  });
+}
+
+function converteParaLibra() {
+  let valorReais = parseFloat(valorEmReais.value);
+  const cotacaoLibra = 6.32;
+  let valorConvertido = valorReais / cotacaoLibra;
+  resultado.innerHTML = valorConvertido.toLocaleString("en-GB", {
+    style: "currency",
+    currency: "USD",
+  });
+}
+
+function converteParaBiticoin() {
+  let valorReais = parseFloat(valorEmReais.value);
+  const cotacaoBiticoin = 114258.6;
+  let valorConvertido = valorReais / cotacaoBiticoin;
+  resultado.innerHTML = valorConvertido.toFixed(6);
+}
+
+(function limpar() {
+  botaoLimpar.onclick = () => {
+    valorEmReais.value = "";
+    resultado.innerHTML = "Digite um valor ,escolha a moeda e converter.";
+  };
+})();
